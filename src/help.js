15fd110b519319e8c220d2f1e07a670e8b0ef2f2
davidlucas2 <80092143+davidlucas2@users.noreply.github.com>
2021-03-06 03:56:19 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,102 @@
+const help = (prefix) => {
+	return `
+
+╔══✪〘 INFO 〙✪══
+║
+╠➥ THOMAS BOT
+╠➥ *1.0* TESTE
+╠➥ 𝐃𝐎𝐍𝐎:  ⃬⃗THOMAS⃖  ☔
+╠➥ *wa.me/+556195993572*
+╠➥ 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+║
+╠══✪〘 FIGU 〙✪══
+║     
+║01 *${prefix}stiker*
+║02 *${prefix}toimg*
+║03 *${prefix}tstiker*
+║
+╠══✪〘 IMAGENS 〙✪══
+║ 
+║04  *${prefix}meme* [inglês]
+║05 *${prefix}memeindo* [indo]
+
+╠══✪〘 OUTROS 〙✪══
+║    
+║06 *${prefix}gtts*[converter texto em fala/áudio]
+║07 *${prefix}loli*[mandar imagens aleatórias de loli]
+➸ útil em : mandar imagens aleatórias de loli
+➸ uso : basta enviar o comando\n
+➸ Comando : *${prefix}nsfwloli*
+➸ útil em : mandar imagens aleatórias de nsfw loli
+➸ uso : basta enviar o comando\n
+➸ Comando : *${prefix}url2img*
+➸ útil em : tirar screenshots da web
+➸ uso : *${prefix}url2img [tipe] [url]*\n
+➸ Comando : *${prefix}simi*
+➸ útil em : responder sua mensagem por simi
+➸ uso : *${prefix}simi sua mensagem*\n
+➸ Comando : *${prefix}ocr*
+➸ útil em : pegar o texto da foto e lhe enviar
+➸ uso : responder imagem ou enviar mensagem com legenda\n
+➸ Comando : *${prefix}wait*
+➸ útil em : pesquisar sobre o anime por imagem [ Que anime é este/que ]
+➸ uso : responder imagem ou enviar imagem com legenda\n
+➸ Comando : *${prefix}setprefix*
+➸ útil em : alterar o prefixo do bot
+➸ uso : *${prefix}setprefix [texto|opcional]*\nexemplo : *${prefix}setprefix ?*
+➸ Nota : Usado somente pelo proprietário do bot\n
+
+       • ─── ✾ ─── •
+       *GRUPO*【✔】
+       • ─── ✾ ─── •
+      
+➸ Comando : *${prefix}linkgroup*
+➸ útil em : enviar o link do grupo
+➸ uso : basta enviar o comando\n
+➸ Comando : *${prefix}marcar*
+➸ útil em : marcar todos os membros do grupo, incluindo administradores
+➸ uso : basta enviar o comando\n
+➸ Nota : Você precisa ser administrador do grupo\n
+➸ Comando : *${prefix}simih*
+➸ útil em : ativar o modo simi no grupo
+➸ uso : *${prefix}simih 1* para ativar o modo simi e *${prefix}simih 0* para 
+➸ desativar o modo simih
+➸ Nota : Você precisa ser administrador do grupo\n
+➸ Comando : *${prefix}add*
+➸ útil em : adicionar membro ao grupo
+➸ uso : *${prefix}add 5585xxxxx*\n
+➸ Nota : o bot precisa ser admin!\n
+➸ Comando : *${prefix}kick*
+➸ útil em : remover membros do grupo
+➸ uso : *${prefix}kick e o @da pessoa*\n
+➸ Nota : Você precisa ser admin e o bot também
+➸ Comando : *${prefix}promote*
+➸ útil em : tornar membro do grupo um administrador
+➸ uso : *${prefix}promote e o @da pessoa*\n
+➸ Nota : Você precisa ser admin e o bot também
+➸ Comando : *${prefix}demote*
+➸ útil em : tornar o administrador um membro comum
+➸ uso : *${prefix}demote e o @da pessoa*\n
+➸ Nota : Você precisa ser admin e o bot também
+
+       • ────── ✾ ────── •
+       *MENU DO ZEUS*【✔】
+       • ────── ✾ ────── •              
+
+➸ *${prefix}help1* ♔
+    
+
+╔════════════════════
+  FEITO POR *ZEUS*
+  DUVIDAS? 👇
+  WA.me/556993899391
+╚════════════════════`
+}
+
+exports.help = help
+
+
+
+
+
+
